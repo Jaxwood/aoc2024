b99f5abb269a408b0732b38dc142698ae5c162c3
Jacob Lorenzen <jacob@lorenzen.me>
2024-12-01 09:24:38 +0100

    chore: parse input

--- src/day01.lua
+++ src/day01.lua
@@ -1,6 +1,19 @@
 local Day01 = {}
 
-function Day01.part1()
+function parse(content)
+    local left = {}
+    local right = {}
+    for i, line in ipairs(content) do
+        l, r = line:match("(%d+)%s+(%d+)")
+        left[i] = l
+        right[i] = r
+    end
+
+    return left, right
+end
+
+function Day01.part1(content)
+    local left, right = parse(content)
     return 0
 end
 
--- tests/day01_test.lua
+++ tests/day01_test.lua
@@ -3,8 +3,18 @@ local Day01 = require('src.day01')
 
 TestLibrary = {}
 
+function readFile(path)
+    local content = {}
+    for line in io.lines(path) do
+        table.insert(content, line)
+    end
+
+    return content
+end
+
 function TestLibrary.test_part1()
-    lu.assertEquals(0, Day01.part1())
+    local content = readFile("data/day01a.txt")
+    lu.assertEquals(11, Day01.part1(content))
 end
 
 os.exit(lu.LuaUnit.run())
