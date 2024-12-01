bb8631764065c27d5301164261ca663437a5599c
Jacob Lorenzen <jacob@lorenzen.me>
2024-12-01 10:04:48 +0100

    feat: solution for day01b

--- src/day01.lua
+++ src/day01.lua
@@ -3,10 +3,10 @@ local Day01 = {}
 function parse(content)
     local left = {}
     local right = {}
-    for i, line in ipairs(content) do
+    for _, line in ipairs(content) do
         l, r = line:match("(%d+)%s+(%d+)")
-        table.insert(left, l)
-        table.insert(right, r)
+        table.insert(left, tonumber(l))
+        table.insert(right, tonumber(r))
     end
 
     table.sort(left)
@@ -21,6 +21,24 @@ function Day01.part1(content)
     for i, v in ipairs(right) do
         result = result + math.abs(left[i] - right[i])
     end
+    return result
+end
+
+function Day01.part2(content)
+    local left, right = parse(content)
+    local result = 0
+
+    -- calculate how many times a number appears in right list
+    local times = {}
+    for _, v in ipairs(right) do
+        times[v] = (times[v] or 0) + 1
+    end
+
+    -- sum all
+    for _, v in ipairs(left) do
+        result = result + (v * (times[v] or 0))
+    end
+
     return result
 end
 
--- tests/day01_test.lua
+++ tests/day01_test.lua
@@ -17,9 +17,19 @@ function TestLibrary.test_parta()
     lu.assertEquals(11, Day01.part1(content))
 end
 
+function TestLibrary.test_partb()
+    local content = readFile("data/day01a.txt")
+    lu.assertEquals(31, Day01.part2(content))
+end
+
 function TestLibrary.test_part1()
     local content = readFile("data/day01.txt")
     lu.assertEquals(2000468, Day01.part1(content))
+end
+
+function TestLibrary.test_part2()
+    local content = readFile("data/day01.txt")
+    lu.assertEquals(18567089, Day01.part2(content))
 end
 
 os.exit(lu.LuaUnit.run())
