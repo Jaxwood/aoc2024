2cb82ca372c10c71804fb9949379c5a0bb6148d6
Jacob Lorenzen <jacob@lorenzen.me>
2024-12-01 09:34:37 +0100

    feat: solution for day01a

--- src/day01.lua
+++ src/day01.lua
@@ -5,16 +5,23 @@ function parse(content)
     local right = {}
     for i, line in ipairs(content) do
         l, r = line:match("(%d+)%s+(%d+)")
-        left[i] = l
-        right[i] = r
+        table.insert(left, l)
+        table.insert(right, r)
     end
+
+    table.sort(left)
+    table.sort(right)
 
     return left, right
 end
 
 function Day01.part1(content)
     local left, right = parse(content)
-    return 0
+    local result = 0
+    for i, v in ipairs(right) do
+        result = result + math.abs(left[i] - right[i])
+    end
+    return result
 end
 
 return Day01
--- tests/day01_test.lua
+++ tests/day01_test.lua
@@ -12,9 +12,14 @@ function readFile(path)
     return content
 end
 
-function TestLibrary.test_part1()
+function TestLibrary.test_parta()
     local content = readFile("data/day01a.txt")
     lu.assertEquals(11, Day01.part1(content))
+end
+
+function TestLibrary.test_part1()
+    local content = readFile("data/day01.txt")
+    lu.assertEquals(2000468, Day01.part1(content))
 end
 
 os.exit(lu.LuaUnit.run())
