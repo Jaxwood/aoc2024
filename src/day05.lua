6b7c1fa257a1784894938cd6f4dcd9ea4c8dfc53
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-05 17:03:07 +0100

    chore(day05): parse input

--- src/day05.lua
+++ src/day05.lua
@@ -1,11 +1,34 @@
 local Day05 = {}
 
 function parse(content)
-    local manual = {}
+    local manual = {
+        ordering = {},
+        updates = {}
+    }
+    local ordering = true
+    for _, line in ipairs(content) do
+        if line == "" then
+            ordering = false
+        elseif ordering then
+            local orderings = {}
+            for order in string.gmatch(line, "(%d+)") do
+                table.insert(orderings, order)
+            end
+            table.insert(manual.ordering, orderings)
+        else
+            local updates = {}
+            for update in string.gmatch(line, "(%d+)") do
+                table.insert(updates, update)
+            end
+            table.insert(manual.updates, updates)
+
+        end
+    end
     return manual
 end
 
 function Day05.part1(content)
+    local manual = parse(content)
     return 0
 end
 
