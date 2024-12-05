d9cb5bff078b4d46dc6f429bf574c1247179dc5c
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-05 20:18:46 +0100

    feat(day05): solution for part 2

--- src/day05.lua
+++ src/day05.lua
@@ -71,6 +71,61 @@ function Day05.part1(content)
             sum = sum + update[(#update // 2) + 1]
         end
     end
+
+    return sum
+end
+
+function Day05.part2(content)
+    local manual = parse(content)
+    local sum = 0
+    local unsorted = {}
+
+    for _, update in ipairs(manual.updates) do
+        -- check if update is in the right order
+        local valid = true
+        local last = 0
+        for idx, val in ipairs(update) do
+            -- store first element in updates
+            if idx == 1 then
+                last = val
+            else
+                -- check if the current element is in the correct order to the rest
+                for i = idx, #update do
+                    if not valid then
+                        break
+                    end
+                    -- if element is not found it should be last
+                    if not manual.ordering[last] then
+                        valid = i ~= #update
+                    -- if element is found it should be in the correct order
+                    else
+                        valid = has_value(manual.ordering[last], update[i])
+                    end
+                end
+                last = val
+            end
+        end
+        if not valid then
+            table.insert(unsorted, update)
+        end
+    end
+
+    for _, update in ipairs(unsorted) do
+        table.sort(update, function(a, b)
+            if not manual.ordering[a] then
+                return false
+            end
+            if not manual.ordering[b] then
+                return true
+            end
+            return has_value(manual.ordering[a], b)
+        end)
+    end
+
+    for _, update in ipairs(unsorted) do
+        sum = sum + update[(#update // 2) + 1]
+    end
+
     return sum
 end
 
--- tests/day05_test.lua
+++ tests/day05_test.lua
@@ -17,9 +17,19 @@ function TestLibrary.test_parta()
     lu.assertEquals(Day05.part1(content), 143)
 end
 
+function TestLibrary.test_partb()
+    local content = readFile("data/day05a.txt")
+    lu.assertEquals(Day05.part2(content), 123)
+end
+
 function TestLibrary.test_part1()
     local content = readFile("data/day05.txt")
     lu.assertEquals(Day05.part1(content), 3608)
+end
+
+function TestLibrary.test_part2()
+    local content = readFile("data/day05.txt")
+    lu.assertEquals(Day05.part2(content), 4922)
 end
 
 os.exit(lu.LuaUnit.run())
