0e895700f7770468a8646611d8986d092975343b
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-06 09:55:50 +0100

    chore(day06): get start location

--- src/day06.lua
+++ src/day06.lua
@@ -2,21 +2,27 @@ local Day06 = {}
 
 function parse(content)
     local map = {}
+    local start = {}
 
     for y, line in ipairs(content) do
         for x = 1, #line do
             if not map[y] then
                 map[y] = {}
             end
-            map[y][x] = line:sub(x, x)
+            local c = line:sub(x, x)
+            if c == '^' then
+                start = { x = x, y = y }
+            end
+            map[y][x] = c
         end
     end
 
-    return map
+    return map, start
 end
 
 function Day06.part1(content)
-    local map = parse(content)
+    local map, start = parse(content)
+    print(start.x, start.y)
     return 0
 end
 
