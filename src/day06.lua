05d3413be70aff5d66f065673940fa506552cef7
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-06 09:52:21 +0100

    chore(day06): parse map

--- src/day06.lua
+++ src/day06.lua
@@ -1,10 +1,22 @@
 local Day06 = {}
 
 function parse(content)
-    return content
+    local map = {}
+
+    for y, line in ipairs(content) do
+        for x = 1, #line do
+            if not map[y] then
+                map[y] = {}
+            end
+            map[y][x] = line:sub(x, x)
+        end
+    end
+
+    return map
 end
 
 function Day06.part1(content)
+    local map = parse(content)
     return 0
 end
 
