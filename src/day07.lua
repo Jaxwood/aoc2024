ebac3df8bf81d85ec9eadb981ddd47f93e9615d6
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-07 07:52:10 +0100

    chore(day07): parse input

--- src/day07.lua
+++ src/day07.lua
@@ -1,13 +1,21 @@
 local Day07 = {}
 
 function parse(content)
-    return content
+    local equations = {}
+    for _, line in ipairs(content) do
+        local numbers = {}
+        local result, nums = line:match("(.*):(.*)")
+        for num in nums:gmatch("(%d+)") do
+            table.insert(numbers, tonumber(num))
+        end
+        table.insert(equations, { result = tonumber(result), numbers = numbers })
+    end
+    return equations
 end
 
 function Day07.part1(content)
+    local data = parse(content)
     return 0
 end
 
-
 return Day07
-
