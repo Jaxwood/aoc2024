ebf27b061ffe8030046d50c3e798b4a3e43638b4
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-10 10:19:50 +0100

    chore(day10): get trailheads

--- src/day10.lua
+++ src/day10.lua
@@ -2,20 +2,24 @@ local Day10 = {}
 
 local function parse(content)
     local map = {}
+    local trailheads = {}
 
     for y = 1, #content do
         for x = 1, #content[y] do
             local c = content[y]:sub(x, x)
             map[y] = map[y] or {}
             map[y][x] = tonumber(c)
+            if c == "0" then
+                table.insert(trailheads, { x = x, y = y })
+            end
         end
     end
 
-    return map
+    return map, trailheads
 end
 
 function Day10.part1(content)
-    local map = parse(content)
+    local map, trailheads = parse(content)
 
     return 0
 end
