e6a5c000f328a2f88722b90b7a9874e9f4ebdec2
Jacob Lorenzen <jacob@lorenzen.me>
2024-12-14 09:39:42 +0100

    chore(day14): improve docs

--- tests/day14_test.lua
+++ tests/day14_test.lua
@@ -23,17 +23,15 @@ function TestLibrary.test_part14_1()
 end
 
 function TestLibrary.test_part14_2()
-    lu,skip("Skip part 2")
     local content = readFile("data/day14.txt")
     -- in part 2 you need to find when the robots groups into a christmas tree
     -- i noticed a pattern each 101 iteration, starting at iteration 82
     -- so i just looped through the iterations by increments of 101 and printed the output to a file
-    -- from scanning the output i found the correct iteration
+    -- from scanning the output i found the correct iteration which for my input was 7051
+    lu.skip("Skip part 2")
     for i = 82, 1000, 101 do
         Day14.part1(content, 101, 103, i)
     end
 end
 
-
--- 7051
 os.exit(lu.LuaUnit.run())
