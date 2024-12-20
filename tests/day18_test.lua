3894b58086ce8527a10bca78e53f04d69a98cf17
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-20 12:42:24 +0100

    chore(day18): solution for part 2

--- tests/day18_test.lua
+++ tests/day18_test.lua
@@ -17,9 +17,19 @@ function TestLibrary.test_part18a()
     lu.assertEquals(Day18.part1(content, 12, 6), 22)
 end
 
+function TestLibrary.test_part18b()
+    local content = readFile("data/day18a.txt")
+    lu.assertEquals(Day18.part2(content, 6), "6,1")
+end
+
 function TestLibrary.test_part18_1()
     local content = readFile("data/day18.txt")
     lu.assertEquals(Day18.part1(content, 1024, 70), 326)
+end
+
+function TestLibrary.test_part18_2()
+    local content = readFile("data/day18.txt")
+    lu.assertEquals(Day18.part2(content, 70), "18,62")
 end
 
 os.exit(lu.LuaUnit.run())
