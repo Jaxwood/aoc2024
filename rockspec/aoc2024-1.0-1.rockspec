e1c4a9eaf6fb13e7d68edc9f243c1fe5b565f629
Jacob Lorenzen <jacob@lorenzen.me>
2024-12-21 08:00:15 +0100

    chore(day20): refactor to use binaryheap package

--- rockspec/aoc2024-1.0-1.rockspec
+++ rockspec/aoc2024-1.0-1.rockspec
@@ -13,7 +13,8 @@ description = {
 
 dependencies = {
     "lua >= 5.4",
-    "luaunit >= 3.4"
+    "luaunit >= 3.4",
+    "binaryheap >= 0.4.1"
 }
 
 build = {
@@ -30,5 +31,14 @@ build = {
        ["aoc2024.Day09"] = "src/day09.lua",
        ["aoc2024.Day10"] = "src/day10.lua",
        ["aoc2024.Day11"] = "src/day11.lua",
+       ["aoc2024.Day12"] = "src/day12.lua",
+       ["aoc2024.Day13"] = "src/day13.lua",
+       ["aoc2024.Day14"] = "src/day14.lua",
+       ["aoc2024.Day15"] = "src/day15.lua",
+       ["aoc2024.Day16"] = "src/day16.lua",
+       ["aoc2024.Day17"] = "src/day17.lua",
+       ["aoc2024.Day18"] = "src/day18.lua",
+       ["aoc2024.Day19"] = "src/day19.lua",
+       ["aoc2024.Day20"] = "src/day20.lua",
    }
 }
--- src/day20.lua
+++ src/day20.lua
@@ -1,3 +1,6 @@
+local binaryheap = require 'binaryheap'
+local unvisited = binaryheap.minUnique()
+
 local Day20 = {}
 
 local OPEN = "."
@@ -36,58 +39,58 @@ local function parse(content)
 end
 
 local neighbors = {
-    { 0, -1 },
-    { 0, 1 },
+    { 0,  -1 },
+    { 0,  1 },
     { -1, 0 },
-    { 1, 0 }
+    { 1,  0 }
 }
+
+local function serialize(x, y)
+    return x .. "_" .. y
+end
+
+local function deserialize(key)
+    local parts = {}
+    for part in string.gmatch(key, "(%d+)") do
+        table.insert(parts, part)
+    end
+    return tonumber(parts[1]), tonumber(parts[2])
+end
 
 function Day20.part1(content)
     local racetrack, from, to = parse(content)
 
-    local unvisited = {}
-    local score = {}
-
     -- initialize the score and unvisited list
     for y, row in ipairs(racetrack) do
-        score[y] = score[y] or {}
         for x, cell in ipairs(row) do
             if cell == 1 then
-                table.insert(unvisited, { x, y })
-                score[y][x] = math.huge
+                unvisited:insert(math.huge, serialize(x, y))
             end
             if x == from[1] and y == from[2] then
-                score[y][x] = 0
+                unvisited:update(serialize(x, y), 0)
             end
         end
     end
 
-    while #unvisited > 0 do
-        -- get the current field with the lowest score
-        table.sort(unvisited, function(a, b)
-            return score[a[2]][a[1]] < score[b[2]][b[1]]
-        end)
-        local current = table.remove(unvisited, 1)
+    -- Dijkstra's algorithm
+    while unvisited:peek() do
+        local current, distance = unvisited:peek()
+        unvisited:remove(current)
 
-        -- if we reached the end, then return the score
-        if current[1] == to[1] and current[2] == to[2] then
-            return score[current[2]][current[1]]
+        -- check if we reached the destination
+        if current == serialize(to[1], to[2]) then
+            return distance
         end
 
-        -- check the neighbors
-        for _, neighbor in ipairs(neighbors) do
-            local x = current[1] + neighbor[1]
-            local y = current[2] + neighbor[2]
-
-            -- check in bounds
-            if x >= 1 and x <= #racetrack[1] and y >= 1 and y <= #racetrack then
-                -- check if the field is open
-                if racetrack[y][x] == 1 then
-                    -- calculate the tentative score and update the score if it's lower
-                    local tentative_score = score[current[2]][current[1]] + 1
-                    if tentative_score < score[y][x] then
-                        score[y][x] = tentative_score
-                    end
+        -- update the distance of the neighbors
+        local new_distance = distance + 1
+        local x, y = deserialize(current)
+        for _, neighbour in ipairs(neighbors) do
+            local key = serialize(x + neighbour[1], y + neighbour[2])
+            if unvisited.reverse[key] then
+                local current_dist = unvisited:valueByPayload(key)
+                if current_dist > new_distance then
+                    unvisited:update(key, new_distance)
                 end
             end
         end
@@ -97,3 +100,4 @@ function Day20.part1(content)
 end
 
 return Day20
+
