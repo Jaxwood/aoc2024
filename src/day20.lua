c3591a7c28dbd154c8f8a224cc42962688b54ef5
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-12-28 20:07:14 +0100

    chore(day20): work on part b

--- src/day20.lua
+++ src/day20.lua
@@ -2,8 +2,8 @@ local binaryheap = require 'binaryheap'
 
 local Day20 = {}
 
-local OPEN = "."
-local WALL = "#"
+local OPEN = 1
+local WALL = 2
 local start = "S"
 local finish = "E"
 
@@ -18,9 +18,9 @@ local function parse(content)
             if racetrack[y] == nil then
                 racetrack[y] = {}
             end
-            if c == OPEN then
+            if c == "." then
                 racetrack[y][x] = 1
-            elseif c == WALL then
+            elseif c == "#" then
                 racetrack[y][x] = 2
             elseif c == start then
                 racetrack[y][x] = 1
@@ -86,7 +86,11 @@ local function dijkstra(unvisited, destination)
     assert(false, "No path found")
 end
 
-function Day20.part1(content)
+local composite_key = function(x, y, xx, yy)
+    return math.min(x, xx) .. "_" .. math.min(y, yy) .. "_" .. math.max(x, xx) .. "_" .. math.max(y, yy)
+end
+
+function Day20.part1(content, seconds, limit)
     local racetrack, from, to = parse(content)
     local destination = serialize(to[1], to[2])
     local unvisited = binaryheap.minUnique()
@@ -94,7 +98,7 @@ function Day20.part1(content)
     -- initialize the unvisited list
     for y, row in ipairs(racetrack) do
         for x, cell in ipairs(row) do
-            if cell == 1 then
+            if cell == OPEN then
                 unvisited:insert(math.huge, serialize(x, y))
             end
 
@@ -107,35 +111,52 @@ function Day20.part1(content)
 
     -- find the cost of each node to reach the destination
     local cost = dijkstra(unvisited, destination)
+    local seen = {}
 
     local total = 0
     -- find all walls can be used as a cheat
     for y, row in ipairs(racetrack) do
         for x, cell in ipairs(row) do
-            -- check if the cell is a wall
-            if cell == 2 and x > 1 and x < #row and y > 1 and y < #racetrack then
-
-                local up = y - 1
-                local down = y + 1
-                -- check if the wall can be used as a cheat in the vertical direction
-                if (racetrack[up][x] == 1 and racetrack[down][x] == 1) then
-                    local diff = math.abs(cost[serialize(x, down)] - cost[serialize(x, up)]) - 2
-                    if diff >= 100 then
-                        total = total + 1
-                    end
-                end
-
-                local left = x - 1
-                local right = x + 1
-                -- check if the wall can be used as a cheat in the horizontal direction
-                if (racetrack[y][left] == 1 and racetrack[y][right] == 1) then
-                    local diff = math.abs(cost[serialize(left, y)] - cost[serialize(right, y)]) - 2
-                    if diff >= 100 then
-                        total = total + 1
+            -- check if the cell is open
+            if cell == OPEN then
+                local key = serialize(x, y)
+                -- check the manhattan distance to the destination
+                -- to verify if the new cell is reachable
+                for yy = y - seconds, y + seconds do
+                    for xx = x - seconds, x + seconds do
+                        local manhattan = math.abs(x - xx) + math.abs(y - yy)
+                        if manhattan <= seconds then
+                            local new_key = serialize(xx, yy)
+                            -- check if the new cell is part of the track
+                            if cost[new_key] and cost[key] then
+                                local diff = math.abs(cost[new_key] - cost[key]) - manhattan
+                                -- check if the difference is greater than the limit
+                                -- store the path to handle the duplicates
+                                if diff >= limit then
+                                    local composite = composite_key(x, y, xx, yy)
+                                    seen[diff] = seen[diff] or {}
+                                    table.insert(seen[diff], composite)
+                                end
+                            end
+                        end
                     end
                 end
             end
         end
+    end
+
+    -- count the number of unique keys for each difference
+    for k, v in pairs(seen) do
+        local unique = {}
+        for _, vv in ipairs(v) do
+            unique[vv] = true
+        end
+        local cnt = 0
+        for _, _ in pairs(unique) do
+            cnt = cnt + 1
+        end
+        total = total + cnt
+        -- print(k, cnt)
     end
 
     return total
--- tests/day20_test.lua
+++ tests/day20_test.lua
@@ -14,12 +14,26 @@ end
 
 function TestLibrary.test_part20a()
     local content = readFile("data/day20a.txt")
-    lu.assertEquals(Day20.part1(content), 0)
+    lu.assertEquals(Day20.part1(content, 2, 0), 296)
+end
+
+function TestLibrary.test_part20b()
+    lu.skip("")
+    local content = readFile("data/day20a.txt")
+    lu.assertEquals(Day20.part1(content, 20, 50), 94)
 end
 
 function TestLibrary.test_part20_1()
     local content = readFile("data/day20.txt")
-    lu.assertEquals(Day20.part1(content), 1293)
+    lu.assertEquals(Day20.part1(content, 2, 100), 1293)
+end
+
+function TestLibrary.test_part20_2()
+    lu.skip("")
+    local content = readFile("data/day20.txt")
+    lu.assertEquals(Day20.part1(content, 20, 100), 9309323)
+    -- too low 669460
+    -- too high 9309323
 end
 
 os.exit(lu.LuaUnit.run())
